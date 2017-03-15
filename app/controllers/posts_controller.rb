424a682fdd6fe769b316dbd4e0b83297356323c0
Stephen Vancott <thevanicotti@gmail.com>
2017-03-15 16:04:15 -0400

    rails 20 checkpoint completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,4 +42,16 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
 end
