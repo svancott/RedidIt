347ef071e4c2cff96818cca1c53199195b4310f7
Stephen Vancott <thevanicotti@gmail.com>
2017-03-16 11:12:28 -0400

    rails 20 assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,15 @@ posts = Post.all
   )
 end
 
+100.times do
+  Question.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  resolved: false
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
