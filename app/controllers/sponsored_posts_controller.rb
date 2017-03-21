5f61f3fdb0ad179c1f593d1c4b76f6941cd05fff
Stephen Vancott <thevanicotti@gmail.com>
2017-03-21 09:15:26 -0400

    rails 22 assignment fixed

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -4,22 +4,27 @@ class SponsoredPostsController < ApplicationController
   end
 
   def show
-     @sponsored_post = SponsoredPost.find(params[:id])
+    @sponsored_post = SponsoredPost.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @sponsored_post = SponsoredPost.new
   end
 
   def create
-    @sponsored_post = SponsoredPost.new
+
+     @sponsored_post = SponsoredPost.new
      @sponsored_post.title = params[:sponsored_post][:title]
      @sponsored_post.body = params[:sponsored_post][:body]
      @sponsored_post.price = params[:sponsored_post][:price]
+     @topic = Topic.find(params[:topic_id])
+
+     @sponsored_post.topic = @topic
 
      if @sponsored_post.save
        flash[:notice] = "Post was saved."
-       redirect_to [@topic, @sponsored_post]
+       redirect_to [@topic]
       else
         flash.now[:alert] = "There was an error saving the post. Please try again."
         render :new
