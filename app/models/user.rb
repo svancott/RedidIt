bfeb25f0232ed5821acd58d1fdc10a672680ba77
Stephen Vancott <thevanicotti@gmail.com>
2017-03-22 10:49:19 -0400

    rails 25 checkpoint completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
