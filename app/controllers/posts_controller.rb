05eb48a2c040a9578c33a51b7fbce1802f0fd0db
Stephen Vancott <thevanicotti@gmail.com>
2017-03-27 13:01:53 -0400

    rails 28 assignment completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,7 +65,7 @@ class PostsController < ApplicationController
 
   def authorize_user
     post = Post.find(params[:id])
-    unless current_user == post.user || current_user.admin?
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
       flash[:alert] = "You must be an admin to do that."
       redirect_to [post.topic, post]
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -47,6 +47,13 @@ member = User.create!(
   password: 'helloworld'
 )
 
+mod = User.create!(
+  name: 'Mod User',
+  email: 'mod@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+)
+
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
