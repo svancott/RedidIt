721dbd15b3af511461c87a825fe2d5f8d06c88db
Stephen Vancott <thevanicotti@gmail.com>
2017-03-28 11:45:06 -0400

    rails 30 checkpoint completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,17 @@ end
 topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
+
 posts = Post.all
 
 100.times do
@@ -53,3 +57,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
