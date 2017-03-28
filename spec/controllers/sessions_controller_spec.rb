e0b09f8a3674127ea606225dd2238e21dc1261b6
Stephen Vancott <thevanicotti@gmail.com>
2017-03-28 09:29:06 -0400

    minor fixes to tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,32 +13,32 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "POST sessions" do
     it "returns http success" do
-      post :create, session: {email: my_user.email}
+      post :create, params: {session: {email: my_user.email}}
       expect(response).to have_http_status(:success)
     end
 
     it "initializes a session" do
-      post :create, session: {email: my_user.email, password: my_user.password}
+      post :create, params: {session: {email: my_user.email, password: my_user.password} }
       expect(session[:user_id]).to eq my_user.id
     end
 
     it "does not add a user id to session due to missing password" do
-      post :create, session: {email: my_user.email}
+      post :create, params: { session: {email: my_user.email} }
       expect(session[:user_id]).to be_nil
     end
 
     it "flashes #error with bad email address" do
-      post :create, session: {email: "does not exist"}
-      expect(flash.now[:aler]).to be_present
+      post :create, params: { session: {email: "does not exist"} }
+      expect(flash.now[:alert]).to be_present
     end
 
     it "renders #new with bad email address" do
-      post :create, session: {email: my_user.email, password: my_user.password}
+      post :create, params: { session: {email: "does not exist"} }
       expect(response).to render_template :new
     end
 
     it "redirects to the root view" do
-      post :create, session: {email: my_user.email, password: my_user.password}
+      post :create, params: { session: {email: my_user.email, password: my_user.password} }
       expect(response).to redirect_to(root_path)
     end
   end
@@ -54,7 +54,7 @@ RSpec.describe SessionsController, type: :controller do
       expect(assigns(:session)).to be_nil
     end
 
-    it "falshes #notice" do
+    it "flashes #notice" do
       delete :destroy, id: my_user.id
       expect(flash[:notice]).to be_present
     end
