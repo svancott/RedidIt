d3f45907bb1ed9a9940d1fb4aa03775a207c187b
Stephen Vancott <thevanicotti@gmail.com>
2017-04-12 16:30:21 -0400

    anyone can now create topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show, :new, :create]
 
   def index
     @topics = Topic.all
